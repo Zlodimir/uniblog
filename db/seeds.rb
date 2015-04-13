3d370e3441642b30c8cc97f0d7af2687398c612b
Zlodimir <sslupinos@gmail.com>
2015-04-13 21:42:05 +0300

    changes for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.exists?(email: "sslupinos@gmail.com") 
+	User.create(email: "sslupinos@gmail.com", password: "main_chain", firstname: "Сергей", lastname: "Лc", confirmed_at: DateTime.now, confirmation_token: "", confirmation_sent_at: DateTime.now)
+end
