a83d7bb717a3afc2b7aec79c2b3b72cbfe3a5a6e
Zlodimir <sslupinos@gmail.com>
2015-04-16 15:17:18 +0300

    changes for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,16 +16,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address => "smtp.mandrillapp.com",
-  :port => "587",
-  :authentication => "plain",
-  :user_name => "sslupinos@gmail.com",
-  :password => "0SaOzB2KdNhJmFNi4E0vHg",
-  :enable_starttls_auto => true 
-  }
-
+  config.action_mailer.delivery_method = :test
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address => 'smtp.mandrillapp.com',,
+  :address => 'smtp.mandrillapp.com',
   :port => '587',
   :authentication => "plain",
   :user_name => ENV['MANDRILL_USERNAME'],
