ea27dd5837b5481e4fbf225c6cbd2554eef571a8
Zlodimir <sslupinos@gmail.com>
2015-04-20 11:55:21 +0300

    changes for heroku

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ActiveRecord::Base
-  belongs_to :author, class_name: "User"
-
   include Bootsy::Container
+  belongs_to :author, class_name: "User"
 
   validates :title, presence: true, length: {minimum: 2}
   validates :description, presence: true, length: {minimum: 2}
