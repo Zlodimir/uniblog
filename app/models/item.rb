b62229e35df3ffd1537753392ea72eaae969b8e6
Zlodimir <sslupinos@gmail.com>
2015-04-20 12:01:39 +0300

    changes for heroku

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
   include Bootsy::Container
   belongs_to :author, class_name: "User"
 
+  mount_uploader :avatar, AvatarUploader
+
   validates :title, presence: true, length: {minimum: 2}
   validates :description, presence: true, length: {minimum: 2}
 
