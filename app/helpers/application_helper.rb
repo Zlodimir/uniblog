66104d544c6cf3ffbd208f60c50bbc23c071cd25
Zlodimir <sslupinos@gmail.com>
2015-04-21 18:17:16 +0300

    dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def admin_only(&block)
 		block.call if current_user.try(:admin)
 	end
+
+	def title(*parts)
+		unless parts.empty?
+			content_for :title do
+				(parts << "Блог").join(" - ")
+			end
+		end
+	end
 end
