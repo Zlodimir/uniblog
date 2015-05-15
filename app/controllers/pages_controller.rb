ecfab94dcae2407ad53dd2b3a3a9186ad51f8bfc
valentin <valentin.zakharov1@gmail.com>
2015-05-16 02:26:37 +0300

    Removed commented code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,6 @@ class PagesController < ApplicationController
   before_action :set_page, only: [:show]
 
   def index
-    #if Page.where(primary: :true).count != 0
-    #  @main_page = Page.where(primary: :true).first
-    #  redirect_to page_path(@main_page)
-    #else
-    #  if Page.count != 0
-    #    @main_page = Page.first
-    #    redirect_to page_path(@main_page)
-    #  else
-    #    redirect_to root_path
-    #  end
-    #end
   end
 
   def show
