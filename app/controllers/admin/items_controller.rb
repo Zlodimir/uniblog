e358a2ad1a635f140f60d9ef23f22b8df163251f
valentin <valentin.zakharov1@gmail.com>
2015-05-16 02:11:37 +0300

    Fixed tab-spaces

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,54 +1,53 @@
 class Admin::ItemsController < Admin::BaseController
-	before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  	def index
-  		@items = Item.order('created_at desc').page params[:page]
-  	end
+  def index
+    @items = Item.order('created_at desc').page params[:page]
+    end
 
-  	def show
+  def show
+  end
 
-  	end
+  def new
+    @item = Item.new
+  end
 
-	def new
-		@item = Item.new
-	end
+  def edit
+  end
 
-	def edit
-	end
+  def update
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item), notice: 'Новость изменена'
+    else
+      flash.now[:alert] = 'Новость не изменена'
+      render 'edit'
+    end
+  end
 
-	def update
-		if @item.update(item_params)
-			redirect_to admin_item_path(@item), notice: 'Новость изменена'
-		else
-			flash.now[:alert] = 'Новость не изменена'
-			render 'edit'
-		end
-	end
+  def create
+    @item = Item.new
+    @item.attributes = item_params
+    @item.author = current_user
 
-	def create
-		@item = Item.new
-		@item.attributes = item_params
-		@item.author = current_user
+    if @item.save
+      redirect_to admin_item_path(@item), notice: 'Новость создана'
+    else
+      flash.now[:alert] ='Новость не создана'
+      render 'new'
+    end
+  end
 
-		if @item.save
-			redirect_to admin_item_path(@item), notice: 'Новость создана'
-		else
-			flash.now[:alert] ='Новость не создана'
-			render 'new'
-		end
-	end
+  def destroy
+    @item.destroy
+    redirect_to admin_items_path, notice: 'Новость удалена'
+  end
 
-	def destroy
-		@item.destroy
-		redirect_to admin_items_path, notice: 'Новость удалена'
-	end
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
-	private
-	def set_item
-		@item = Item.find(params[:id])
-	end
-
-	def item_params
-		params.require(:item).permit(:title, :description, :bootsy_image_gallery_id)
-	end
+  def item_params
+    params.require(:item).permit(:title, :description, :bootsy_image_gallery_id)
+  end
 end
